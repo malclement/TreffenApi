0b0771a32ca1cbc6639e0d77ad8d44dcf167883a
arthurpellegrin <arthurpellegrin1104@gmail.com>
2022-02-08 23:56:30 +0100

    POST example

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field, EmailStr
+from bson import ObjectId
 import pymongo
-import os
+
 uri = 'mongodb+srv://artpel:artyty@treffendb.esk4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 client = pymongo.MongoClient(uri)
-db = client.treffendb
-
-
+db = client.students
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class StudentModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jane Doe",
+                "email": "jdoe@example.com",
+            }
+        }
+
+
+@app.post("/", response_description="Add new student", response_model=StudentModel)
+async def create_student(student: StudentModel = Body(...)):
+    student = jsonable_encoder(student)
+    new_student = await db["students"].insert_one(student)
+    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
