ee8298ac16e90eb288e8ad982dd9731035c762b9
arthurpellegrin <arthurpellegrin1104@gmail.com>
2022-02-09 23:38:27 +0100

    POST example

---
+++ app/api.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, Body, Depends
+from app.model import UserSchema, UserLoginSchema
+from app.auth.auth_bearer import JWTBearer
+from app.auth.auth_handler import signJWT
+from fastapi.encoders import jsonable_encoder
+
+import pymongo
+
+uri = 'mongodb+srv://artpel:artyty@treffendb.esk4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = pymongo.MongoClient(uri)
+db = client.Users
+
+app = FastAPI()
+
+
+app = FastAPI()
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to Treffen API!."}
+
+
+@app.post("/user/signup", tags=["user"])
+async def create_user(user: UserSchema = Body(...)):
+    valid = await check_signup(user)
+    if valid:
+        user = jsonable_encoder(user)
+        new_user = db["Users"].insert_one(user)
+        return signJWT(user['email'])
+    else:
+        return {"error" : "User Already Exist"}
+
+
+@app.post("/user/login", tags=["user"])
+async def user_login(user: UserLoginSchema = Body(...)):
+    valid = await check_user(user)
+    if valid:
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
+
+
+@app.get("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
+async def read_post() -> dict:
+    return {"message": "My protected Post"}
+
+
+async def check_user(data: UserLoginSchema):
+    for results in db.Users.find({"email": data.email}):
+        if results['password'] == data.password:
+            return True
+        else:
+            return False
+
+
+async def check_signup(data: UserSchema):
+    for results in db.Users.find({"email": data.email}):
+        if results['email'] != "":
+            return False
+    return True
--- main.py
+++ main.py
@@ -1,52 +1,52 @@
-from fastapi import FastAPI, Body, status
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel, Field, EmailStr
-from bson import ObjectId
-import pymongo
+# from fastapi import FastAPI, Body, status
+# from fastapi.responses import JSONResponse
+# from fastapi.encoders import jsonable_encoder
+# from pydantic import BaseModel, Field, EmailStr
+# from bson import ObjectId
+#
+#
+# class PyObjectId(ObjectId):
+#     @classmethod
+#     def __get_validators__(cls):
+#         yield cls.validate
+#
+#     @classmethod
+#     def validate(cls, v):
+#         if not ObjectId.is_valid(v):
+#             raise ValueError("Invalid objectid")
+#         return ObjectId(v)
+#
+#     @classmethod
+#     def __modify_schema__(cls, field_schema):
+#         field_schema.update(type="string")
+#
+#
+# class StudentModel(BaseModel):
+#     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+#     name: str = Field(...)
+#     email: EmailStr = Field(...)
+#
+#     class Config:
+#         allow_population_by_field_name = True
+#         arbitrary_types_allowed = True
+#         json_encoders = {ObjectId: str}
+#         schema_extra = {
+#             "example": {
+#                 "name": "Jane Doe",
+#                 "email": "jdoe@example.com",
+#             }
+#         }
+#
+#
+# @app.post("/", response_description="Add new student", response_model=StudentModel)
+# async def create_student(student: StudentModel = Body(...)):
+#     student = jsonable_encoder(student)
+#     new_student = await db["students"].insert_one(student)
+#     created_student = await db["students"].find_one({"_id": new_student.inserted_id})
+#     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
 
-uri = 'mongodb+srv://artpel:artyty@treffendb.esk4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-client = pymongo.MongoClient(uri)
-db = client.students
-app = FastAPI()
 
+import uvicorn
 
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
-
-
-class StudentModel(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    name: str = Field(...)
-    email: EmailStr = Field(...)
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "name": "Jane Doe",
-                "email": "jdoe@example.com",
-            }
-        }
-
-
-@app.post("/", response_description="Add new student", response_model=StudentModel)
-async def create_student(student: StudentModel = Body(...)):
-    student = jsonable_encoder(student)
-    new_student = await db["students"].insert_one(student)
-    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
