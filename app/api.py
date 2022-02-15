700d4ec6cf2315a9c9971197384551a05fb40292
malclement <clement.malige1@gmail.com>
2022-02-15 16:35:24 +0100

    Specificity

--- app/api.py
+++ app/api.py
@@ -1,17 +1,18 @@
+import json
+
+from bson import json_util
 from fastapi import FastAPI, Body, Depends, HTTPException
 from app.model import UserSchema, UserLoginSchema
 from app.auth.auth_bearer import JWTBearer
 from app.auth.auth_handler import signJWT
 from fastapi.encoders import jsonable_encoder
+from urllib.parse import unquote
 
 import pymongo
 
 uri = 'mongodb+srv://artpel:artyty@treffendb.esk4d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 client = pymongo.MongoClient(uri)
 db = client.Users
-
-app = FastAPI()
-
 
 app = FastAPI()
 
@@ -39,9 +40,27 @@ async def user_login(user: UserLoginSchema = Body(...)):
         return signJWT(user.email)
     raise HTTPException(status_code=500, detail="wrong Login")
 
+
 @app.get("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
 async def read_post() -> dict:
     return {"message": "My protected Post"}
+
+
+# allows to serialize ObjectId
+# used in @app.get("/user/info/{user_email}", tags=["user"])
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+
+# return user info from is email
+@app.get("/user/info/{user_email}", tags=["user"])
+async def user_info(user_email: str):
+    user_email = unquote(user_email)
+    print(user_email)
+    json_results = []
+    for results in db.Users.find({"email": user_email}):
+        json_results.append(results)
+    return parse_json(json_results[0])
 
 
 async def check_user(data: UserLoginSchema):
--- app/model.py
+++ app/model.py
@@ -1,17 +1,22 @@
-from pydantic import BaseModel, Field, EmailStr
+import uuid
+
+from pydantic import BaseModel, Field, EmailStr, UUID4
 
 
 class UserSchema(BaseModel):
+    id: UUID4 = Field(default_factory=uuid.uuid4)
     fullname: str = Field(...)
     email: EmailStr = Field(...)
     password: str = Field(...)
+    friends: list = []
 
     class Config:
         schema_extra = {
             "example": {
                 "fullname": "Abdulazeez Abdulazeez Adeshina",
                 "email": "abdulazeez@x.com",
-                "password": "weakpassword"
+                "password": "weakpassword",
+                "friends": ["id1", "id2"]
             }
         }
 
